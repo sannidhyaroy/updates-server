8c82b81f5162120e7cae01b7e0afcfd11e6459ad
Sannidhya <58990408+sannidhyaroy@users.noreply.github.com>
2025-02-16 14:37:29 +0530

    feat: install zen as root binary

--- install.sh
+++ install.sh
@@ -3,18 +3,25 @@
 set -euo pipefail
 
 app_name=zen
-literal_name_of_installation_directory=".tarball-installations"
-universal_path_for_installation_directory="$HOME/$literal_name_of_installation_directory"
+literal_name_of_installation_directory="src"
+universal_path_for_installation_directory="/usr/local/$literal_name_of_installation_directory"
 app_installation_directory="$universal_path_for_installation_directory/zen"
 official_package_location="https://github.com/zen-browser/desktop/releases/latest/download/zen.linux-x86_64.tar.xz"
+#official_package_location="https://github.com/zen-browser/desktop/releases/download/<version>/zen.linux-x86_64.tar.xz"
 tar_location=$(mktemp /tmp/zen.XXXXXX.tar.xz)
 open_tar_application_data_location="zen"
-local_bin_path="$HOME/.local/bin"
-local_application_path="$HOME/.local/share/applications"
-app_bin_in_local_bin="$local_bin_path/$app_name"
-desktop_in_local_applications="$local_application_path/$app_name.desktop"
+root_bin_path="/usr/bin"
+root_application_path="/usr/share/applications"
+app_bin_in_root_bin="$root_bin_path/$app_name"
+desktop_in_local_applications="$root_application_path/$app_name.desktop"
 icon_path="$app_installation_directory/browser/chrome/icons/default/default128.png"
 executable_path=$app_installation_directory/zen
+
+# Check if the script is being run as root
+if [ "$EUID" -ne 0 ]; then
+    echo "This script must be run as root."
+    exit 1
+fi
 
 echo "Welcome to Zen tarball installer, just chill and wait for the installation to complete!"
 
@@ -35,9 +42,9 @@ tar -xvJf $tar_location
 echo "Untarred successfully!"
 
 echo "Checking to see if an older installation exists"
-if [ -f "$app_bin_in_local_bin" ]; then
+if [ -f "$app_bin_in_root_bin" ]; then
   echo "Old bin file detected, removing..."
-  rm "$app_bin_in_local_bin"
+  rm "$app_bin_in_root_bin"
 fi
 
 if [ -d "$app_installation_directory" ]; then
@@ -46,7 +53,7 @@ if [ -d "$app_installation_directory" ]; then
 fi
 
 if [ -f "$desktop_in_local_applications" ]; then
-  echo "Old app files are found, removing..."
+  echo "Old desktop files are found, removing..."
   rm "$desktop_in_local_applications"
 fi
 
@@ -61,21 +68,21 @@ echo "Zen successfully moved to your safe place!"
 
 rm $tar_location
 
-if [ ! -d $local_bin_path ]; then
-  echo "$local_bin_path not found, creating it for you"
-  mkdir $local_bin_path
+if [ ! -d $root_bin_path ]; then
+  echo "$root_bin_path not found, creating it for you"
+  mkdir $root_bin_path
 fi
 
-touch $app_bin_in_local_bin
-chmod u+x $app_bin_in_local_bin
+touch $app_bin_in_root_bin
+chmod u+x $app_bin_in_root_bin
 echo "#!/bin/bash
-$executable_path" >> $app_bin_in_local_bin
+$executable_path" >> $app_bin_in_root_bin
 
 echo "Created executable for your \$PATH if you ever need"
 
-if [ ! -d $local_application_path ]; then
-  echo "Creating the $local_application_path directory for desktop file"
-  mkdir $local_application_path
+if [ ! -d $root_application_path ]; then
+  echo "Creating the $root_application_path directory for desktop file"
+  mkdir $root_application_path
 fi
 
 
